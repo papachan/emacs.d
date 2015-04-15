77583b8100163be6450e2007e2aec1f4fa94ab85
dan loaiza <papachan@gmail.com>
2015-04-14 21:15:24 -0500

    Update snippet.el

--- papachan/snippet.el
+++ papachan/snippet.el
@@ -4,6 +4,5 @@
   (interactive)
   (insert "<!DOCTYPE html>\n<html>\n<head>\n    <title></title>\n</head>\n<body>\n\n</body>\n</html>"))
 (global-set-key "\C-cv" 'snippet-html)
-;(global-set-key (kbd "C-c C-v") 'snippet-html)
 
 
