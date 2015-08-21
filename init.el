4dc0dabe80cad80381a355e701992037a77698ce
Dan Loaiza <papachan@gmail.com>
2015-08-21 15:17:43 -0500

    adding flx-ido mode

--- init.el
+++ init.el
@@ -29,7 +29,8 @@
                 "github.el"
                 "appearance.el"
                 "setup-python"
-                "setup-cider"))
+                "setup-cider"
+                "ido-snippet"))
   (load (concat dotfiles-lisp-dir file)))
 
 ; emacs-server
