d1f62cc4bce709d31643d20625d03ce77da5f39f
papachan <danloaiza@yahoo.com>
2015-10-04 20:30:49 -0500

    updating webmode.el

--- papachan/webmode.el
+++ papachan/webmode.el
@@ -3,11 +3,18 @@
 (require 'web-mode)
 (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+
+(use-package web-mode
+  :config
+  (progn
+    (setq web-mode-markup-indent-offset 2)
+    (setq web-mode-css-indent-offset 2)
+    (setq web-mode-extra-snippets
+      '(("erb" . (("toto" . ("<% toto | %>\n\n<% end %>"))))
+        ("php" . (("dowhile" . ("<?php do { ?>\n\n<?php } while (|); ?>"))
+                  ("debug" . ("<?php error_log(__LINE__); ?>"))))
+       ))))
 
 (provide 'webmode)
 ;;; webmode.el ends here
