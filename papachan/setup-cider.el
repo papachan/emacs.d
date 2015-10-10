9c07928cd2833ae0d51100411875bf9c4a8343e0
papachan <danloaiza@yahoo.com>
2015-10-10 11:55:31 -0500

    adding cider from melpa-stable

--- papachan/setup-cider.el
+++ papachan/setup-cider.el
@@ -1,7 +1,7 @@
 ;; clojure setup
 
 ;; Clojure IDE and REPL for Emacs
-(add-to-list 'load-path "~/.emacs.d/vendor/cider")
+;(add-to-list 'load-path "~/.emacs.d/vendor/cider")
 (require 'cider)
 ;; autocompletion
 (require 'company)
