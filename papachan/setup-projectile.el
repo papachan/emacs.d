8e9f417ce319a3e12fadc38a2f5a0075f4030c18
papachan <danloaiza@yahoo.com>
2015-10-11 20:53:56 -0500

    adding setup-projectile file

---
+++ papachan/setup-projectile.el
@@ -0,0 +1,19 @@
+;;; setup-projectile.el
+;(require-package 'projectile)
+
+(projectile-global-mode)
+
+(defun file-fuzzy-finder ()
+  "Open files with projectile-find-file or projectile-find-file-in-directory"
+  (interactive)
+  (if (projectile-project-p)
+    (projectile-find-file)
+    (ido-find-file)))
+
+(setq projectile-completion-system 'helm)
+(helm-projectile-on)
+
+(global-set-key (kbd "C-p") 'file-fuzzy-finder)
+
+(provide 'setup-projectile)
+;;; setup-projectile.el ends here
