bf5dd0a4ea804b0d4e86d9674d65bf45e27115c3
papachan <danloaiza@yahoo.com>
2015-10-17 07:06:33 -0500

    replace path to vendor with expand-file-name

--- papachan/setup-python.el
+++ papachan/setup-python.el
@@ -1,11 +1,13 @@
 ;; python setup
 
 ;; python mode
-(setq py-install-directory "~/.emacs.d/vendor/python-mode.el-6.2.0")
+(setq py-install-directory
+      (expand-file-name "vendor/python-mode.el-6.2.0"  dotemacs-dir))
 (add-to-list 'load-path py-install-directory)
 (require 'python-mode)
 
-(load-file "~/.emacs.d/vendor/emacs-for-python/epy-init.el")
+(load-file
+ (expand-file-name "vendor/emacs-for-python/epy-init.el"  dotemacs-dir))
 (require 'epy-setup)
 (require 'epy-python)
 (require 'epy-completion)
