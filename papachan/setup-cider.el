458db362789af9004975cd4f248e4b1a140179e4
papachan <danloaiza@yahoo.com>
2015-11-17 18:24:08 -0500

    adding hookos for tdd with cider

--- papachan/setup-cider.el
+++ papachan/setup-cider.el
@@ -5,29 +5,49 @@
 (require 'cider)
 ;; autocompletion
 (require 'company)
-;; REPL history file
-(setq cider-repl-history-file
-      (expand-file-name "cider-history" dotemacs-dir))
-;; nice pretty printing
-(setq cider-repl-use-pretty-printing t)
-;; nicer font lock in REPL
-(setq cider-repl-use-clojure-font-lock t)
-;; result prefix for the REPL
-(setq cider-repl-result-prefix ";; => ")
-;; never ending REPL history
-(setq cider-repl-wrap-history t)
-;; looong history
-(setq cider-repl-history-size 3000)
-;; eldoc for clojure
-(add-hook 'cider-mode-hook #'eldoc-mode)
-;; error buffer not popping up
-(setq cider-show-error-buffer nil)
-;; company mode for completion
-;(add-hook 'cider-repl-mode-hook #'company-mode)
-;(add-hook 'cider-mode-hook #'company-mode)
-;; paredit-mode
-(add-hook 'clojure-mode-hook 'paredit-mode)
 
-(setq company-idle-delay nil) ; never start completions automatically
-(global-set-key (kbd "M-TAB") #'company-complete) ; use meta+tab, aka C-M-i, as manual trigger
+(defun tdd-test ()
+  "Thin wrapper around `cider-test-run-tests'."
+  (when (cider-connected-p)
+    (cider-test-run-tests nil)))
+
+(define-minor-mode tdd-mode
+  "Run all tests whenever a file is saved."
+  nil " TDD" nil
+  (if tdd-mode
+      (add-hook 'after-save-hook #'tdd-test nil 'local)
+    (remove-hook 'after-save-hook #'tdd-test 'local)))
+
+(use-package cider
+  :config
+  (progn
+    ;; REPL history file
+    (setq cider-repl-history-file
+          (expand-file-name "cider-history" dotemacs-dir))
+    ;; nice pretty printing
+    (setq cider-repl-use-pretty-printing t)
+    ;; nicer font lock in REPL
+    (setq cider-repl-use-clojure-font-lock t)
+    ;; result prefix for the REPL
+    (setq cider-repl-result-prefix ";; => ")
+    ;; never ending REPL history
+    (setq cider-repl-wrap-history t)
+    ;; looong history
+    (setq cider-repl-history-size 3000)
+    ;; eldoc for clojure
+    (add-hook 'cider-mode-hook #'eldoc-mode)
+    ;; error buffer not popping up
+    (setq cider-show-error-buffer nil)
+    ; add hook for tdd with cider
+	(add-hook 'cider-mode-hook #'tdd-mode)
+	(add-hook 'cider-repl-mode-hook #'tdd-mode)
+  	; never start completions automatically
+    (setq company-idle-delay nil)
+    ;; company mode for completion
+    ;(add-hook 'cider-repl-mode-hook #'company-mode)
+    ;(add-hook 'cider-mode-hook #'company-mode)
+    (global-set-key (kbd "M-TAB") #'company-complete) ; meta+tab, aka C-M-i, as manual trigger
+    ;; paredit-mode
+    (add-hook 'clojure-mode-hook 'paredit-mode)))
+
 ;;; setup-cider.el ends here
