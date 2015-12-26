65acc4f207cceedd9559d85f2265b869059d5903
Dan Loaiza <papachan@gmail.com>
2015-12-26 08:25:18 -0500

    adding slack client

--- init.el
+++ init.el
@@ -69,6 +69,7 @@
 (add-to-list 'load-path "~/.emacs.d/vendor/origami.el")
 (add-to-list 'load-path "~/.emacs.d/vendor/emacs-neotree")
 (add-to-list 'load-path "~/.emacs.d/vendor/x5o.el")
+(add-to-list 'load-path "~/.emacs.d/vendor/slack")
 (add-to-list 'load-path "~/.emacs.d/lib")
 
 ; default HOME directory
