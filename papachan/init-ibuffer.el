9bc41e96769b8525acfefa8586253ce22d6690fd
Dan Loaiza <papachan@gmail.com>
2016-03-21 20:39:36 -0500

    update init-ibuffer settings with filter-groups

--- papachan/init-ibuffer.el
+++ papachan/init-ibuffer.el
@@ -3,6 +3,19 @@
 
 (eval-after-load 'ibuffer
   (progn
+    (setq ibuffer-saved-filter-groups
+          '(("home"
+             ("emacs-config" (or (filename . ".emacs.d")
+                                 (filename . "emacs-config")))
+             ("Shell" (name . "\*shell\*"))
+             ("SQL" (name . "\*SQL"))
+             ("code" (or (filename . "source_code")
+                         (mode . ruby-mode)
+                         (name . "lisp")))
+             ("Org"  (mode . org-mode))
+             ("Magit" (or (name . "magit")
+                          (mode . magit-status-mode))))))
+
     (define-ibuffer-sorter filename-or-dired
       "Sort the buffers by their pathname."
       (:description "filenames plus dired")
@@ -24,13 +37,12 @@
     (define-key ibuffer-mode-map (kbd "K") 'ibuffer-do-kill-lines)
     (define-key ibuffer-mode-map [tab] 'next-line)
     (define-key ibuffer-mode-map [(shift tab)] 'previous-line)
-    (define-key ibuffer-mode-map (kbd "C-g") 'quit-window)
+    (define-key ibuffer-mode-map (kbd "C-g") 'quit-window)))
 
-    (add-hook
-     'ibuffer-mode-hook
-     '(lambda()
-        (define-key ibuffer-mode-map (kbd "s p") 'ibuffer-do-sort-by-filename-or-dired)))))
-
+(add-hook 'ibuffer-mode-hook
+          '(lambda ()
+             (ibuffer-switch-to-saved-filter-groups "home")
+             (define-key ibuffer-mode-map (kbd "s p") 'ibuffer-do-sort-by-filename-or-dired)))
 
 (provide 'init-buffer)
 ;;; init-ibuffer.el ends here
