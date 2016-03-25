7741ea4c64b5da9154d912ec9994016b4bf8ae5c
Dan Loaiza <papachan@gmail.com>
2016-03-25 10:49:25 -0500

    update download file method

--- lisp/bootstrap.el
+++ lisp/bootstrap.el
@@ -1,23 +1,16 @@
 ;;; bootstrap.el
-(setq elisp-file-list (list '("lorem-ipsum.el"
-                    "http://raw.github.com/jschaf/emacs-lorem-ipsum/master/lorem-ipsum.el")
-                  '("insert-time.el"
-                    "http://raw.github.com/rmm5t/insert-time.el/master/insert-time.el")
-                  '("iflipb.el"
-                    "http://raw.github.com/jrosdahl/iflipb/master/iflipb.el")
-                  '("directory-files-recursive.el"
-                    "https://gist.githubusercontent.com/dmgerman/5675462/raw/1af30f73c7d1f246dd69efe3cbfc327f160fca1a/directory-files-recursive.el")
-                  '("twittering-mode.el"
-                    "http://raw.github.com/hayamiz/twittering-mode/master/twittering-mode.el")
-                  '("helm-aws.el"
-                    "http://raw.github.com/istib/helm-aws/master/helm-aws.el")))
+(setq elisp-file-list '("http://raw.github.com/jschaf/emacs-lorem-ipsum/master/lorem-ipsum.el"
+                        "http://raw.github.com/rmm5t/insert-time.el/master/insert-time.el"
+                        "http://raw.github.com/jrosdahl/iflipb/master/iflipb.el"
+                        "https://gist.githubusercontent.com/dmgerman/5675462/raw/1af30f73c7d1f246dd69efe3cbfc327f160fca1a/directory-files-recursive.el"
+                        "http://raw.github.com/hayamiz/twittering-mode/master/twittering-mode.el"
+                        "http://raw.github.com/istib/helm-aws/master/helm-aws.el"))
 
 (defun download-elisp-files (list)
   (while list
-    (let* ((n (car (car list)))
-          (url (car (cdr (car list))))
+    (let ((url (car list))
           (dir (concat dotemacs-dir "lib/")))
-      (unless (file-exists-p (concat dir n))
+      (unless (file-exists-p (concat dir (url-file-nondirectory url)))
         (url-copy-file url (concat dir (url-file-nondirectory url)))))
     (setq list (cdr list))))
 
