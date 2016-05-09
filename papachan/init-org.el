a0e410bade434173a75107a202d02118b7c6e018
Dan Loaiza <papachan@gmail.com>
2016-05-08 19:26:34 -0500

    update org mode setup

--- papachan/init-org.el
+++ papachan/init-org.el
@@ -1,9 +1,22 @@
 ;;; init-org.el
 ;;
-(add-to-list 'org-emphasis-alist
-             (quote
-              ("*" (:foreground "green"))))
+(require 'org)
 
+(add-to-list 'org-emphasis-alist
+             '("*" (:foreground "green")))
+(add-to-list 'org-emphasis-alist
+             '("/" (italic :foreground "yellow")))
+(add-to-list 'org-emphasis-alist
+             '("_" (underline :foreground "black" :background "CadetBlue2")))
+(add-to-list 'org-emphasis-alist
+             '("+" (:strike-through t :foreground "red")))
+(add-to-list 'org-emphasis-alist
+             '("~" org-code verbatim))
+(add-to-list 'org-emphasis-alist
+             '("=" org-verbatim verbatim))
+
+
+(add-to-list 'auto-mode-alist '("\\.\\(org\\|txt\\|doc\\)\\'" . org-mode))
 
 (provide 'init-org)
 ;; init-org.el ends here
