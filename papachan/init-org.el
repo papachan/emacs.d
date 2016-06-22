6ff8d1d39ca0804d7b5bc1417c0f05e115666774
Dan Loaiza <papachan@gmail.com>
2016-06-21 20:01:46 -0500

    set up org mode

--- papachan/init-org.el
+++ papachan/init-org.el
@@ -1,6 +1,11 @@
 ;;; init-org.el
 ;;
 (require 'org)
+;; Remove the markup characters
+(setq org-hide-emphasis-markers t)
+
+;; Turn on Visual-line-mode
+(add-hook 'org-mode-hook 'turn-on-visual-line-mode)
 
 (dolist (org-styles '(("*" (:foreground "green"))
       ("/" (italic :foreground "yellow"))
