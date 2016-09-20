c1137709d32605251bf59ad29d1adef65dd2fb03
dan loaiza <papachan@gmail.com>
2016-09-19 22:58:43 -0500

    support autocomplete 1.5.1

--- init.el
+++ init.el
@@ -55,11 +55,11 @@
     (setq x-select-enable-clipboard t))))
 
 ;; autocomplete  git@github.com:auto-complete/auto-complete.git
-(add-to-list 'load-path "~/.emacs.d/vendor/auto-complete-1.4.0")
+(add-to-list 'load-path "~/.emacs.d/vendor/auto-complete-1.5.1")
 (require 'auto-complete)
 (require 'auto-complete-config)
 (add-to-list 'ac-dictionary-directories
-             (concat user-emacs-directory "vendor/auto-complete-1.4.0/dict"))
+             (concat user-emacs-directory "vendor/auto-complete-1.5.1/dict"))
 (ac-config-default)
 (setq ac-ignore-case nil)
 (global-auto-complete-mode t)
