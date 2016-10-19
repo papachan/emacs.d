90ecb0b493914ba377e2ca647b1ac728f7f938b6
Dan Loaiza <papachan@gmail.com>
2016-10-18 19:59:21 -0500

    update magit setup with magit 2.10 version

--- papachan/setup-magit.el
+++ papachan/setup-magit.el
@@ -26,6 +26,43 @@
     (magit-visit-item)
     (delete-current-buffer-file)
     (magit-refresh))
+
+  ;; magit-status popup set it up with only staged and unstaged changes
+  (define-derived-mode magit-staging-mode magit-status-mode "Magit staging"
+    "Mode for showing staged and unstaged changes."
+    :group 'magit-status)
+
+  (defun magit-staging-refresh-buffer ()
+    (magit-insert-section (status)
+      (magit-insert-unstaged-changes)
+      (magit-insert-staged-changes)))
+
+  (defun magit-staging ()
+    (interactive)
+    (magit-mode-setup #'magit-staging-mode))
+
+  ;; Protect against accident pushes to upstream
+  (defadvice magit-push-current-to-upstream
+      (around my-protect-accidental-magit-push-current-to-upstream)
+    "Protect against accidental push to upstream.
+     Causes `magit-git-push' to ask the user for confirmation first."
+    (let ((my-magit-ask-before-push t))
+      ad-do-it))
+
+  (defadvice magit-git-push (around my-protect-accidental-magit-git-push)
+    "Maybe ask the user for confirmation before pushing.
+     Advice to `magit-push-current-to-upstream' triggers this query."
+    (if (bound-and-true-p my-magit-ask-before-push)
+        ;; Arglist is (BRANCH TARGET ARGS)
+        (if (yes-or-no-p (format "Push %s branch upstream to %s? "
+                                 (ad-get-arg 0) (ad-get-arg 1)))
+            ad-do-it
+          (error "Push to upstream aborted by user"))
+      ad-do-it))
+
+  (ad-activate 'magit-push-current-to-upstream)
+  (ad-activate 'magit-git-push)
+
   (progn
     (define-key magit-status-mode-map (kbd "C-x C-k") 'magit-kill-file-on-line)
     (define-key magit-status-mode-map (kbd "C-c e") 'magit-log-buffer-file)
