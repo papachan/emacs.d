5cc8a80f90c57f7ce36da55b0206be2ee8ab6573
Dan Loaiza <papachan@gmail.com>
2016-11-05 11:25:13 -0500

    update dired filters

--- papachan/setup-dired.el
+++ papachan/setup-dired.el
@@ -1,4 +1,12 @@
 ;;; setup-dired --- define a new method after loading dired
+(defun dired-find-directory ()
+  "A `dired-find-file' which only works on directories."
+  (interactive)
+  (let ((find-file-run-dired t)
+        (file (dired-get-file-for-visit)))
+    (when (file-directory-p file)
+      (find-file file))))
+
 (eval-after-load 'dired
   '(progn
      (cond
@@ -7,6 +15,8 @@
       (t (setq dired-listing-switches "--si -al")))
      ;; (t (setq dired-listing-switches "-lX --si --group-directories-first"))
      (define-key dired-mode-map (kbd "C-c n") 'my-dired-create-file)
+     (define-key dired-mode-map (kbd "<left>") 'dired-jump)
+     (define-key dired-mode-map (kbd "<right>") 'dired-find-file)
      (defun my-dired-create-file (file)
        "Create a file called FILE. If FILE already exists, signal an error."
        (interactive
