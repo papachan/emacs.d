ff0b56c1fc77e82b8490a2e8f2e74df3100e8749
Dan Loaiza <dan.loaiza@easytaxi.com.br>
2016-11-20 09:18:51 -0500

    disable clj-refactor warnings

--- papachan/setup-cider.el
+++ papachan/setup-cider.el
@@ -9,6 +9,7 @@
 (use-package clj-refactor
   :ensure t
   :config (progn (setq cljr-suppress-middleware-warnings t)
+                 (setq cljr-warn-on-eval nil)
                  (add-hook 'clojure-mode-hook (lambda ()
                         (clj-refactor-mode 1)
                         (cljr-add-keybindings-with-prefix "C-c C-m")))))
