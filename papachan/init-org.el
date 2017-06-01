51af75c908625cd61c00083f9b815876841c11a4
Dan Loaiza <dan.loaiza@easytaxi.com>
2017-06-01 10:55:13 -0500

    update init org-mode disabling strikethrough char

--- papachan/init-org.el
+++ papachan/init-org.el
@@ -1,24 +1,20 @@
 ;;; init-org.el
 ;;
 (require 'org)
-;; Remove the markup characters
-(setq org-hide-emphasis-markers t)
+(setq org-emphasis-alist (delete '("+" (:strike-through t)) org-emphasis-alist))
 
 ;; Turn on Visual-line-mode
 (add-hook 'org-mode-hook 'turn-on-visual-line-mode)
-(add-hook 'org-mode-hook
-          (lambda ()
-            (setq org-enable-table-editor nil)))
 
-(dolist (org-styles '(("*" (:foreground "green"))
-      ("/" (italic :foreground "yellow"))
-      ("_" (underline :foreground "black" :background "CadetBlue2"))
-      ("+" (:strike-through t :foreground "red"))
-      ("~" org-code verbatim)
-      ("=" org-verbatim verbatim)))
+(dolist (org-styles
+         '(("*" (:foreground "green"))
+           ("/" (italic :foreground "yellow"))
+           ("_" (underline :foreground "black" :background "CadetBlue2"))
+           ;; ("+" (:strike-through t :foreground "gray"))
+           ("~" org-code verbatim)
+           ("=" org-verbatim verbatim)))
     (add-to-list 'org-emphasis-alist
              org-styles))
-
 
 (add-to-list 'auto-mode-alist '("\\.\\(org\\|txt\\|doc\\)\\'" . org-mode))
 
