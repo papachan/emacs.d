77eac7fb0ec48e429c270a5ec363a0147545665d
Dan Loaiza <papachan@gmail.com>
2017-11-13 07:53:18 -0500

    better invocation with Lorem-ipsum-insert-paragraphs

--- papachan/shortcuts.el
+++ papachan/shortcuts.el
@@ -79,7 +79,7 @@
 
 ;; comment line
 (global-set-key (kbd "C-<f4>") (kbd "C-u 80 #"))
-(global-set-key (kbd "C-<f5>") 'Lorem-ipsum-insert-paragraphs)
+(global-set-key (kbd "C-<f5>") (lambda()  (interactive) (Lorem-ipsum-insert-paragraphs 200)))
 (global-set-key (kbd "C-<f6>") 'snippet-html)
 (global-set-key (kbd "C-<f7>") 'lorem-ipsum-insert-list)
 (global-set-key (kbd "C-x M-d") 'insert-current-date)
