4190f23b36339616cc272caa0a12667cdaf4b42e
Dan Loaiza <papachan@gmail.com>
2019-08-16 10:10:21 -0500

    refactor

--- papachan/setup-python.el
+++ papachan/setup-python.el
@@ -1,15 +1,17 @@
-;; python setup
-
-;; python mode
-(setq python-indent-offset 4)
-(setq py-install-directory
-      (expand-file-name "vendor/python-mode.el"  dotemacs-dir))
-(add-to-list 'load-path py-install-directory)
+(require 'yasnippet)
 (require 'python-mode)
+
+(use-package python-mode
+  :ensure t
+  :config (setq python-indent-offset 4))
+
+(use-package python
+	:mode ("\\.py" . python-mode))
 
 (defun clean-python-files ()
   (interactive)
   (shell-command "find . -name '*.py[co]' -delete")
+  (shell-command "find . -name __py*__ -print0 | xargs -0 rm -rf")
   (revert-buffer t t))
 
 ; create empty __init__.py at the place
@@ -17,27 +19,6 @@
   (interactive)
   (shell-command "touch __init__.py"))
 
-(load-file
- (expand-file-name "vendor/emacs-for-python/epy-init.el"  dotemacs-dir))
-(require 'epy-setup)
-(setq epy-enable-ropemacs nil)
-(require 'epy-python)
-(require 'epy-completion)
-(require 'epy-editing)
-(require 'epy-bindings)
-(require 'epy-nose)
-(require 'yasnippet)
-
-;; setting pyflake for python check style
-(when (executable-find "pyflakes")
-  (epy-setup-checker "pyflakes %f"))
-(epy-setup-ipython)
-(global-hl-line-mode t)
-(global-set-key (kbd "C-c !") 'python-shell-switch-to-shell)
-(global-set-key (kbd "C-x C-g") 'clean-python-files)
-(global-set-key (kbd "C-c M-a") 'create-empty-init-py)
-
-;; (setq flymake-log-level 3)
 
 (provide 'setup-python)
 ;;; setup-python.el ends here
