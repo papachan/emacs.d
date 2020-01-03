44670fe685e600fdcc39581627693c4921929963
Dan Loaiza <papachan@gmail.com>
2020-01-03 16:59:41 -0500

    add some customizations to undo-tree

--- papachan/setup-undo.el
+++ papachan/setup-undo.el
@@ -7,9 +7,14 @@
   :diminish undo-tree-mode
   :bind ("C-z" . undo-tree-visualize)
   :init
-  (global-undo-tree-mode))
+  (global-undo-tree-mode)
+
+  ;; Each node in the undo tree should have a timestamp.
+  (setq undo-tree-visualizer-timestamps t)
+
+  ;; Show a diff window displaying changes between undo nodes.
+  (setq undo-tree-visualizer-diff t))
 
 
 (provide 'setup-undo)
 ;;; setup-undo.el ends here
-
