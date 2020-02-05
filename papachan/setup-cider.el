5a799d4a456f9943c54708cb81a0a26ff76caa5e
Dan Loaiza <papachan@gmail.com>
2020-02-05 16:22:44 -0500

    disable warm ast with clj-refactor

--- papachan/setup-cider.el
+++ papachan/setup-cider.el
@@ -11,6 +11,7 @@
   :ensure t
   :config (progn (setq cljr-suppress-middleware-warnings t)
                  (setq cljr-warn-on-eval nil)
+                 (setq cljr-eagerly-build-asts-on-startup nil)
                  (add-hook 'clojure-mode-hook (lambda ()
                         (clj-refactor-mode 1)
                         (cljr-add-keybindings-with-prefix "C-c C-m")))))
