47c7af47e08b13ba5f0c3b4caf3ed0f125a580f0
Dan Loaiza <papachan@gmail.com>
2020-05-06 08:15:37 -0500

    update configs before upgrading to emacs27

--- papachan/appearance.el
+++ papachan/appearance.el
@@ -17,10 +17,13 @@
   (when (eq system-type 'gnu/linux)
     (load-file (expand-file-name "vendor/zerodark-theme/zerodark-theme.el" dotemacs-dir))
     (setq zerodark-use-high-contrast-in-mode-line nil)
-    (let ((x 78))
-      ;; transparent frame
-      (set-frame-parameter (selected-frame) 'alpha (cons x '(50)))
-      (add-to-list 'default-frame-alist '(alpha . (cons x '(50)))))))
+    (let ((opacity '(87 87)))
+      (set-frame-parameter (selected-frame) 'alpha opacity))
+    ;; (let ((x 78))
+    ;;   ;; transparent frame
+    ;;   (set-frame-parameter (selected-frame) 'alpha (cons x '(50)))
+    ;;   (add-to-list 'default-frame-alist '(alpha . (cons x '(50)))))
+    ))
 
 (provide 'appearance)
 
