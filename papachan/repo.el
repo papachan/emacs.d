d122a4f0ace73a9037b11c25e0f2261e1c26195a
Dan Loaiza <papachan@gmail.com>
2020-05-12 11:38:36 -0500

    removing old files

--- papachan/repo.el
+++
@@ -1,5 +0,0 @@
-;; packages
-(dolist (source '(("melpa" . "https://melpa.org/packages/")
-                  ("org" . "http://orgmode.org/elpa/")))
-     (add-to-list 'package-archives source t))
-(package-initialize)
