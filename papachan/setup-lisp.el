c3bb35026f33372b486e36ce6c34812cab490719
Dan Loaiza <papachan@gmail.com>
2020-05-12 16:38:31 -0500

    setup lisp

--- papachan/setup-lisp.el
+++ papachan/setup-lisp.el
@@ -1,52 +1,15 @@
 ;;; setup-lisp.el
 ;;
 (require 'slime)
-(require 'slime-company)
-
-(setq common-lisp-hyperspec-root
-      (concat "file://" (expand-file-name "~/common-lisp/HyperSpec/")))
 
 (use-package lisp-mode
-  :mode (("\\.lisp'" . lisp-mode)
+  :mode (("\\.el'" . lisp-mode)
+         ("\\.lisp'" . lisp-mode)
          ("\\.lsp'" . lisp-mode)
          ("\\.cl'" . lisp-mode)
          ("\\.asd'" . lisp-mode)
-         ("\\.fasl'" . lisp-mode)))
-
-(use-package slime-company
-  :ensure t)
-
-(use-package slime
-  :ensure t
-  :init (progn (setq slime-contribs '(slime-fancy)
-                     inferior-lisp-program (if (eq system-type 'darwin)
-                                               "/usr/local/bin/sbcl"
-                                             "sbcl")
-                     slime-lisp-implementations
-                     '((sbcl ("/usr/local/bin/sbcl") :coding-system utf-8-unix))
-                     slime-net-coding-system 'utf-8-unix))
-  :config (progn
-            (let ((helper-file (expand-file-name "~/.local/opt/quicklisp/slime-helper.el")))
-              (if (file-exists-p helper-file)
-                  (load helper-file)
-                (warn "(ql:quickload \"quicklisp-slime-helper\") must be run in quicklisp before")))
-            (slime-setup '(slime-company))
-            (global-set-key (kbd "C-c C-q") 'slime-repl-quit)
-            (global-set-key (kbd "C-c s") 'slime-selector)))
-
-
-(defun hook-lisp-mode ()
-  "Display lambda as λ"
-  (global-prettify-symbols-mode 1))
-
-(add-hook 'lisp-mode-hook #'enable-paredit-mode)
-(add-hook 'slime-repl-mode-hook #'enable-paredit-mode)
-(eval-after-load "auto-complete"
-  '(add-to-list 'ac-modes 'slime-repl-mode))
-
-(custom-set-variables '(safe-local-variable-values (quote ((Syntax . Common-Lisp)))))
-
-(setq slime-protocol-version 'ignore)
+         ("\\.fasl'" . lisp-mode))
+  :hook ((lisp-mode slime-repl-mode). enable-paredit-mode))
 
 (provide 'setup-lisp)
 ;; setup-lisp.el ends here
