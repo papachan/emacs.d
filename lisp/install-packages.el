a1976b9d78fd6697a62529f388efd5cbbbea1b87
Dan Loaiza <papachan@gmail.com>
2020-05-13 16:01:17 -0500

    update package list

--- lisp/install-packages.el
+++ lisp/install-packages.el
@@ -1,108 +1,110 @@
-(dolist (package '(alchemist
+(dolist (package '(a
+                   alchemist
                    all-the-icons
-                   clj-refactor
+                   ansi
+                   async
+                   bind-key
+                   cask
                    cider
+                   clj-refactor
                    clojure-mode
                    color-theme
+                   commander
+                   company
                    composable
+                   dash
+                   dired-hacks-utils
                    dired-sidebar
                    dired-subtree
-                   dired-hacks-utils
                    elfeed
                    elixir-mode
                    enh-ruby-mode
+                   epl
                    erlang
                    expand-region
+                   f
+                   flycheck
                    flycheck-clj-kondo
                    flycheck-elixir
-                   flycheck
+                   gh
+                   git
+                   git-commit
                    gnu-elpa-keyring-update
                    graphql-mode
                    haskell-emacs
                    haskell-mode
-                   helm-projectile
                    helm
                    helm-core
+                   helm-projectile
+                   hl-todo
+                   ht
                    htmlize
                    hydra
                    idle-highlight-mode
                    iflipb
                    inf-ruby
                    inflections
+                   ivy
                    kotlin-mode
                    let-alist
-                   magit-gh-pulls
-                   gh
                    logito
+                   macrostep
+                   magit
+                   magit-gh-pulls
                    magit-org-todos
                    magit-todos
-                   magit
-                   git-commit
-                   hl-todo
                    markdown-mode
                    marshal
-                   ht
                    maxframe
                    memoize
                    move-text
                    multiple-cursors
+                   package-build
+                   pallet
                    paredit
-                   parseedn
                    parseclj
-                   a
+                   parseedn
                    pcache
                    pcre2el
                    php-mode
-                   popup
-                   projectile
                    pkg-info
+                   popup
+                   powerline
+                   projectile
+                   python-mode
                    pyvenv
                    queue
                    rg
+                   rich-minority
+                   s
                    seq
                    sesman
-                   slime-company
-                   company
+                   shut-up
                    slime
-                   macrostep
-                   smart-mode-line-powerline-theme
+                   slime-company
                    smart-mode-line
-                   rich-minority
-                   powerline
+                   smart-mode-line-powerline-theme
                    smartparens
                    spinner
                    swiper
-                   ivy
                    transient
                    typescript-mode
                    undo-tree
+                   use-package
                    visual-regexp
                    web-mode
                    wgrep
                    window-numbering
                    with-editor
-                   async
                    xterm-color
                    yaml-mode
-                   yasnippet-snippets
                    yasnippet
-                   commander
-                   git
-                   pallet
-                   cask
-                   ansi
-                   package-build
-                   epl
-                   f
-                   dash
-                   s
-                   shut-up
-                   use-package
-                   bind-key)))
+                   yasnippet-snippets))
+  (package-install package))
 
-(progn (package-refresh-contents)
-       (package-install-selected-packages)
-       (byte-recompile-directory package-user-dir nil 'force))
+;; (progn (package-refresh-contents)
+;;        (package-install-selected-packages)
+;;        (byte-recompile-directory package-user-dir nil 'force))
 
 (defun package-reinstall-all-activated-packages ()
   "Refresh and reinstall all activated packages."
@@ -117,89 +119,5 @@
 (dolist (package-name package-activated-list)
   (message "%S" package-name))
 
-(dolist (package-name package-activated-list)
-  (package-install package-name))
-
-
-(dolist (package '(clojure-mode
-                   color-theme
-                   composable
-                   dired-sidebar
-                   dired-subtree
-                   dired-hacks-utils
-                   elfeed
-                   elixir-mode
-                   enh-ruby-mode
-                   erlang
-                   expand-region
-                   flycheck-clj-kondo
-                   flycheck-elixir
-                   flycheck
-                   helm-projectile
-                   helm-core
-                   htmlize
-                   hydra
-                   idle-highlight-mode
-                   iflipb
-                   inf-ruby
-                   inflections
-                   kotlin-mode
-                   let-alist
-                   magit-gh-pulls
-                   gh
-                   logito
-                   git-commit
-                   markdown-mode
-                   marshal
-                   maxframe
-                   memoize
-                   move-text
-                   multiple-cursors
-                   paredit
-                   parseedn
-                   parseclj
-                   a
-                   pcache
-                   pcre2el
-                   php-mode
-                   popup
-                   projectile
-                   pkg-info
-                   pyvenv
-                   queue
-                   rg
-                   seq
-                   sesman
-                   slime-company
-                   company
-                   macrostep
-                   smart-mode-line-powerline-theme
-                   smart-mode-line
-                   rich-minority
-                   powerline
-                   smartparens
-                   spinner
-                   swiper
-                   ivy
-                   transient
-                   typescript-mode
-                   undo-tree
-                   visual-regexp
-                   web-mode
-                   wgrep
-                   window-numbering
-                   with-editor
-                   async
-                   xterm-color
-                   yaml-mode
-                   yasnippet-snippets
-                   commander
-                   git
-                   cask
-                   ansi
-                   package-build
-                   epl
-                   dash
-                   shut-up
-                   origami))
-  (package-install package))
+;; (dolist (package-name package-activated-list)
+;;   (package-install package-name))
