f585c7c168d74e2d48040407efe6c1e3835825ca
Dan Loaiza <papachan@gmail.com>
2020-05-14 19:11:39 -0500

    update eshell configs

--- papachan/setup-eshell.el
+++ papachan/setup-eshell.el
@@ -14,10 +14,17 @@
         (erase-buffer)))
 
     (global-set-key  (kbd "C-x E") 'eshell-new)
-    (global-set-key  (kbd "C-l") 'eshell/clear))
+    (global-set-key  (kbd "C-l") 'eshell/clear)
+
+    (add-hook 'eshell-mode-hook (lambda ()
+                                  'ansi-color-for-comint-mode-on
+                                  (setenv "PATH" (shell-command-to-string "source ~/.zshenv; echo -n $PATH"))
+                                  (setq-local show-trailing-whitespace nil)
+                                  (define-key eshell-mode-map (kbd "M-p") 'helm-eshell-history))))
   :init
-  (setq eshell-mv-overwrite-files nil))
+  (progn (setq eshell-mv-overwrite-files nil)
+         (setq eshell-banner-message (concat " Welcome back " user-login-name ".\n"))
+         (setq eshell-aliases-file (expand-file-name "eshell/alias" dotemacs-dir))))
 
 (provide 'setup-eshell)
-
 ;;; setup-eshell.el ends here
