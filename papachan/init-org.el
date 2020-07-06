d3c9368a4f0f10e5cd787eaac74f5fd0f6126386
Dan Loaiza <dan@resonancias.org>
2020-07-06 10:23:12 -0500

    setup clojure with org-babel-mode

--- papachan/init-org.el
+++ papachan/init-org.el
@@ -1,6 +1,9 @@
 ;;; init-org.el
 ;;
 (require 'org)
+(require 'ob-clojure)
+(require 'cider)
+
 (setq org-emphasis-alist (delete '("+" (:strike-through t)) org-emphasis-alist))
 
 ;; Turn on Visual-line-mode
@@ -17,6 +20,19 @@
              org-styles))
 
 (add-to-list 'auto-mode-alist '("\\.\\(org\\|doc\\)\\'" . org-mode))
+
+(setq org-babel-clojure-backend 'cider)
+
+(setq org-confirm-babel-evaluate nil)
+
+(org-babel-do-load-languages
+  'org-babel-load-languages
+  '((lisp . t)
+    (emacs-lisp . t)
+    (python . t)
+    (calc . t)
+    (clojure . t)
+    (shell . t)))
 
 (provide 'init-org)
 ;; init-org.el ends here
