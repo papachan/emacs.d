02fb8eafa69ca59c62064bd1743074a203cc7e74
Dan Loaiza <papachan@gmail.com>
2022-10-21 17:49:02 -0500

    add projectile

---
+++ papachan/setup-projectile.el
@@ -0,0 +1,36 @@
+;;; setup-projectile.el --- projectile
+;;; Commentary:
+
+;;; Code:
+(require 'projectile)
+;; (require 'helm-projectile)
+
+(defun file-fuzzy-finder ()
+  "Open files with projectile-find-file or projectile-find-file-in-directory."
+  (interactive)
+  (if (projectile-project-p)
+    (projectile-find-file)
+    (ido-find-file)))
+
+(use-package projectile
+  :ensure t
+  :defer t
+  :bind (("C-c p p" . projectile-switch-project)
+         ("C-c p f" . file-fuzzy-finder))
+  :config
+  (projectile-global-mode 1)
+  (setq projectile-enable-caching nil
+        projectile-track-known-projects-automatically nil)
+  (add-to-list 'projectile-globally-ignored-files "*.min.js")
+  (add-to-list 'projectile-globally-ignored-directories "js/compiled")
+  (add-to-list 'projectile-globally-ignored-directories "resources/public/js")
+  (add-to-list 'projectile-globally-ignored-directories "node[_-]modules")
+  (add-to-list 'projectile-globally-ignored-directories "target")
+  (setq projectile-mode-line '(:eval (format " Prj[%s]" (projectile-project-name)))))
+
+;; (use-package helm-projectile
+;;   :ensure t
+;;   :config (helm-projectile-on))
+
+(provide 'setup-projectile)
+;;; setup-projectile.el ends here
