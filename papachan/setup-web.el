d02c66944653d2d03bcc56e89c241d6f2834b8f3
Dan Loaiza <dan@resonancias.org>
2023-09-12 17:11:15 -0500

    add new setting

---
+++ papachan/setup-web.el
@@ -0,0 +1,18 @@
+;;; setup-web.el --- Summary
+;;; Commentary:
+;;; Code:
+(use-package typescript-mode
+  :config
+  ;; we choose this instead of tsx-mode so that eglot can automatically figure out language for server
+  ;; see https://github.com/joaotavora/eglot/issues/624 and https://github.com/joaotavora/eglot#handling-quirky-servers
+  (define-derived-mode typescriptreact-mode typescript-mode
+    "TypeScript TSX")
+
+  ;; use our derived mode for tsx files
+  (add-to-list 'auto-mode-alist '("\\.tsx?\\'" . typescriptreact-mode))
+  ;; by default, typescript-mode is mapped to the treesitter typescript parser
+  ;; use our derived mode to map both .tsx AND .ts -> typescriptreact-mode -> treesitter tsx
+  (add-to-list 'tree-sitter-major-mode-language-alist '(typescriptreact-mode . tsx)))
+
+(provide 'setup-web)
+;;; setup-web.el ends here
