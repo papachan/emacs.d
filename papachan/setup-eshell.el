9c78e177c6fa4b505d28e3cb344aa23f8f763a88
Dan Loaiza <dan@resonancias.org>
2024-06-05 13:53:50 -0500

    refactor eshell aliases

--- papachan/setup-eshell.el
+++ papachan/setup-eshell.el
@@ -2,6 +2,20 @@
 ;;; Commentary:
 
 ;;; Code:
+(require 'eshell)
+(require 'em-alias)
+
+(eshell/alias "d" "dired $1")
+(eshell/alias "cls" "clear")
+(eshell/alias "Dd" "cd ~/Desktop")
+(eshell/alias "ff" "find-file $1")
+(eshell/alias "FF" "find-file-other-window $1")
+(eshell/alias "ppo" "helm-projectile")
+(eshell/alias "ppf" "projectile-find-file")
+(eshell/alias "sc" "cd ~/dev/source_code")
+(eshell/alias "clj-repl" "clojure -Sdeps '{:deps {nrepl/nrepl {:mvn/version \"1.1.2\"} cider/cider-nrepl {:mvn/version \"0.48.0\"} refactor-nrepl/refactor-nrepl {:mvn/version \"3.10.0\"}} :aliases {:cider/nrepl {:main-opts [\"-m\" \"nrepl.cmdline\" \"--middleware\" \"[cider.nrepl/cider-middleware]\"]}}}' -M:cider/nrepl -h 0.0.0.0 -p $1")
+(eshell/alias "open" "explorer.exe .")
+
 (defun eshell-new ()
   (interactive)
   (eshell t))
