b86d7b9f38e28f4f3e3930982ec2a0e32a302871
Dan Loaiza <dan@resonancias.org>
2024-11-25 17:29:09 -0500

    update chatgpt

--- papachan/setup-chatgpt.el
+++ papachan/setup-chatgpt.el
@@ -7,8 +7,21 @@
 (use-package chatgpt-shell
   :ensure t
   :init
-  (setq chatgpt-shell-chatgpt-model-version "gpt-3.5-turbo-0613")
-  (setq chatgpt-shell-chatgpt-streaming t)
+  (setq shell-maker-prompt-before-killing-buffer nil)
+  ;; (setq chatgpt-shell-google-key "my-key")
+  ;; (setq chatgpt-shell-anthropic-key "my-key)"
+  (setq chatgpt-shell-chatgpt-model-version "o1-mini")
+  ;; chatgpt-4o-latest
+  ;; o1-preview
+  ;; o1-mini
+  ;; gpt-4o
+  ;; claude-3-5-sonnet-20240620
+  ;; gemini-1.5-pro-latest
+  ;; gemini-1.5-flash
+  ;; llama3.2
+  ;; llama3.2:1b
+  ;; gemma2:2b
+
   :custom
   ((chatgpt-shell-openai-key
     (lambda ()
