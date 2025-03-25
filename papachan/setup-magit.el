85d0cdfb85eadb0c3808b4b4e1a66cf1db8ad85f
Dan Loaiza <dan@resonancias.org>
2025-03-25 08:29:43 -0500

    remove magit defadvice because obsolete macros

--- papachan/setup-magit.el
+++ papachan/setup-magit.el
@@ -32,29 +32,7 @@
   (add-to-list 'magit-process-password-prompt-regexps
                "^\\(Enter \\)?[Pp]assphrase for [^ ]*: ?$")
   (add-to-list 'magit-process-password-prompt-regexps
-               "^Bad passphrase, try again for [^ ]*: ?$")
-
-  ;; Protect against accident pushes to upstream
-  (defadvice magit-push-current-to-upstream
-      (around my-protect-accidental-magit-push-current-to-upstream)
-    "Protect against accidental push to upstream.
-     Causes `magit-git-push' to ask the user for confirmation first."
-    (let ((my-magit-ask-before-push t))
-      ad-do-it))
-
-  (defadvice magit-git-push (around my-protect-accidental-magit-git-push)
-    "Maybe ask the user for confirmation before pushing.
-     Advice to `magit-push-current-to-upstream' triggers this query."
-    (if (bound-and-true-p my-magit-ask-before-push)
-        ;; Arglist is (BRANCH TARGET ARGS)
-        (if (yes-or-no-p (format "Push %s branch upstream to %s? "
-                                 (ad-get-arg 0) (ad-get-arg 1)))
-            ad-do-it
-          (error "Push to upstream aborted by user"))
-      ad-do-it))
-
-  (ad-activate 'magit-push-current-to-upstream)
-  (ad-activate 'magit-git-push))
+               "^Bad passphrase, try again for [^ ]*: ?$"))
 
 (provide 'setup-magit)
 ;;; setup-magit.el ends here
