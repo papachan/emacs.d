3e8f94e3191f934e194ef70b28160951f70e1adf
Dan Loaiza <dan@resonancias.org>
2025-04-11 16:57:22 -0500

    web-mode

--- papachan/setup-web.el
+++ papachan/setup-web.el
@@ -1,13 +1,37 @@
 ;;; setup-web.el --- -*- lexical-binding: t -*-
 ;;; Commentary:
+
 ;;; Code:
-(setq js-indent-level 2)
+(use-package web-mode
+  :ensure t
+  :defer t
+  :bind ("C-c C-;" . web-mode-comment-or-uncomment)
+  :mode ("\\.html\\'" . web-mode)
+  :config
+  (setq js-indent-level 2
+        web-mode-markup-indent-offset 2
+        web-mode-css-indent-offset 2
+        web-mode-code-indent-offset 2))
+
 (add-to-list 'auto-mode-alist '("\\.yaml$" . yaml-mode))
-(add-to-list 'auto-mode-alist '("\\.json$" . javascript-mode))
-(add-to-list 'auto-mode-alist '("\\.tsx$" . typescript-mode))
+
+(use-package javascript
+  :defer t
+  :commands javascript-mode
+  :mode (("\\.json\\'" . javascript-mode)
+         ("\\.jsx\\'" . javascript-mode)
+         ("\\.mjs\\'" . javascript-mode))
+  :init
+  :config
+  (custom-set-variables
+   '(js-indent-level 2)))
 
 (use-package typescript-mode
-  :defer t)
+  :defer t
+  :mode (("\\.ts\\'" . typescript-mode)
+         ("\\.tsx\\'" . typescript-mode))
+  :config
+  (setq typescript-indent-level 2))
 
 
 (provide 'setup-web)
