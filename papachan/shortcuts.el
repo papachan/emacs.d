8ab5c2fab4e2db3419c67bc3bbb06549e81d953f
Dan Loaiza <dan@resonancias.org>
2025-08-11 07:44:30 -0500

    new functions

--- papachan/shortcuts.el
+++ papachan/shortcuts.el
@@ -62,8 +62,9 @@
 (define-key global-map (kbd "C-c r") 'vr/replace)
 (define-key global-map (kbd "C-c q") 'vr/query-replace)
 (define-key global-map (kbd "C-c q") 'vr/query-replace)
-(global-set-key (kbd "C-c %") 'query-replace-regexp)
+;; (global-set-key (kbd "C-c %") 'query-replace-regexp)
 (global-set-key (kbd "C-c C-v") 'other-frame)
+(global-set-key (kbd "C-c i") 'copy-backward-word)
 
 ;; (global-set-key (kbd "M-g 1") (lambda () (interactive) (insert-into-buffer "~/tmp/gitconfig.txt")))
 ;; (global-set-key (kbd "M-g 2") (lambda () (interactive) (insert-into-buffer "~/tmp/gitconfig-papachan.txt")))
