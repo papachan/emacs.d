3bf6239305d1bd17f07dbfe1cab5dd1238df9b6d
Dan Loaiza <dan@resonancias.org>
2026-02-24 15:09:13 -0500

    update ivy swiper

--- papachan/setup-ivy.el
+++ papachan/setup-ivy.el
@@ -1,7 +1,8 @@
 ;;; setup-ivy.el --- -*- lexical-binding: t -*-
 ;;; Commentary:
+
 ;;; Code:
-(require 'swiper)
+
 ;; ivy-mode
 (use-package ivy
   :ensure t
@@ -14,7 +15,10 @@
   :config
   (ivy-mode 1))
 
-(global-set-key "\C-s" 'swiper)
-(global-set-key "\C-r" 'swiper)
+(use-package swiper
+  :ensure t
+  :bind (("C-s" . swiper)
+         ("C-r" . swiper)))
+
 (provide 'setup-ivy)
 ;;; setup-ivy.el ends here
