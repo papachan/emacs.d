a02563966098a1bee5d4901caa43ab38c1280f7e
Dan Loaiza <dan@resonancias.org>
2026-02-27 11:15:30 -0500

    setup swiper update

--- papachan/setup-swiper.el
+++ papachan/setup-swiper.el
@@ -1,44 +1,23 @@
 ;;; setup-swiper.el --- Summary
 ;;; Commentary:
+
 ;;; Code:
+;; ivy-mode
 (use-package ivy
+  :ensure t
   :bind (("C-c C-r" . ivy-resume)
          ("<f6>" . ivy-resume))
   :init (setq ivy-use-virtual-buffers t
               enable-recursive-minibuffers t)
+        ;; enable this if you want `swiper' to use it
+        ;; (setq search-default-mode #'char-fold-to-regexp)
   :config
   (ivy-mode 1))
 
-(require 'swiper)
-(global-set-key "\C-s" 'swiper)
-(global-set-key "\C-r" 'swiper)
-;; (use-package ivy
-;;   :bind (("C-c C-r" . ivy-resume))
-;;   :config
-;;   (require 'swiper)
-;;   (ivy-mode)
+(use-package swiper
+  :ensure t
+  :bind (("C-s" . swiper)
+         ("C-r" . swiper)))
 
-;;   (setq ivy-use-virtual-buffers t
-;;         enable-recursive-minibuffers t
-;;         ivy-height 13
-;;         ivy-count-format "%d/%d "
-;;         ivy-virtual-abbreviate 'full ; Show the full virtual file paths
-;;         ivy-extra-directories nil ; default value: ("../" "./")
-;;         ivy-wrap t
-;;         ivy-action-wrap t
-;;         ivy-use-selectable-prompt t)
-
-;;   ;; modify default search behaviour of ivy
-;;   (setq ivy-re-builders-alist
-;;         '((t . ivy--regex-plus)))
-
-;;   (bind-keys
-;;    :map ivy-occur-grep-mode-map
-;;    ("n" . ivy-occur-next-line)
-;;    ("p" . ivy-occur-previous-line)
-;;    ("b" . backward-char)
-;;    ("f" . forward-char)
-;;    ("v" . ivy-occur-press) ; default f
-;;    ("RET" . ivy-occur-press)))
 (provide 'setup-swiper)
 ;;; setup-swiper.el ends here
