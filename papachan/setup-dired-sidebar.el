ff2451a13d1052d0b2edc47937d7ad8ab5b8ab92
Dan Loaiza <dan@resonancias.org>
2026-08-25 08:55:18 -0500

    fix dired-sidebar upgrade

--- papachan/setup-dired-sidebar.el
+++ papachan/setup-dired-sidebar.el
@@ -9,13 +9,25 @@
 (use-package dired-sidebar
   :ensure t
   :bind (("S-<f8>" . dired-sidebar-toggle-sidebar))
-  :hook ((dired-mode . all-the-icons-dired-mode)
-         (dired-sidebar-mode-hook . (lambda ()
-                                      (tab-line-mode -1))))
+  :init
+  (add-hook 'dired-sidebar-mode-hook
+            (lambda ()
+              (unless (file-remote-p default-directory)
+                (auto-revert-mode))))
   :config
-  (add-hook 'dired-sidebar-mode-hook (lambda ()
-                                       (tab-line-mode -1)))
+  (push 'toggle-window-split dired-sidebar-toggle-hidden-commands)
+  (push 'rotate-windows dired-sidebar-toggle-hidden-commands)
   :commands (dired-sidebar-toggle-sidebar))
+
+;; dired-subtree gives the actual tree-expand behavior
+(use-package dired-subtree
+  :ensure t
+  :after dired
+  :bind (:map dired-mode-map
+              ("<tab>" . dired-subtree-toggle)
+              ("TAB"   . dired-subtree-toggle)
+              ("<backtab>" . dired-subtree-cycle)))
+
 
 (provide 'setup-dired-sidebar)
 ;;; setup-dired-sidebar.el ends here
