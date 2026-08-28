9f6050067fe062e777a48f7b3db87f03ccb1beef
Dan Loaiza <dan@resonancias.org>
2026-08-28 10:16:41 -0500

    update projectile

--- papachan/setup-projectile.el
+++ papachan/setup-projectile.el
@@ -1,4 +1,4 @@
-;;; setup-projectile.el --- projectile
+;;; setup-projectile.el --- projectile. -*- lexical-binding: nil; -*-
 ;;; Commentary:
 
 ;;; Code:
@@ -18,6 +18,7 @@
   :bind (("C-c p p" . projectile-switch-project)
          ("C-c p f" . file-fuzzy-finder))
   :config
+  (projectile-mode +1)
   (setq projectile-enable-caching nil
         projectile-track-known-projects-automatically nil)
   (add-to-list 'projectile-globally-ignored-files "*.min.js")
